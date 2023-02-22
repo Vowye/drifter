212fdd4a2e418d94449c3f5d950c295684f5c5ac
Kaynã Camargo <kayna.khad@gmail.com>
2023-02-21 21:50:13 -0300

    fix: update user migration

--- backend/db/migrate/20221222070729_create_users.rb
+++ backend/db/migrate/20221222070729_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.string :cpf
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :digest_password, null: false
 
       t.timestamps
     end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_091609) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_070729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "digest_password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
