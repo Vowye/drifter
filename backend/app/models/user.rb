297b894062fbea07f70fb464041d8dd005410453
Kaynã Camargo <kayna.khad@gmail.com>
2023-02-21 21:54:57 -0300

    feat: update user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {message: "Email should be unique"}
+  validates :password, presence: true, confirmation: {case_sensitive: true}, length: { minimum: 10 }
+  validates :password_confirmation, presence: true
 end
